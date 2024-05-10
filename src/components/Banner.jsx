12b338fd0b7ea88b23bda2bfccc5b833e62b8606
Soundar Rajan <soundar1309@gmail.com>
2024-05-10 15:03:22 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import bannerImg from "/images/home/banner.png";
 import { useTheme } from "../hooks/ThemeContext";
@@ -122,8 +123,7 @@ const Banner = () => {
             Dive into Delights Of Delectable <span className="text-green">Food</span>
           </h2>
           <p className="text-[#4A4A4A] text-xl">
-            Where Each Plate Weaves a Story of Culinary Mastery and Passionate
-            Craftsmanship
+          Indulge in the savory delights of Italian cuisine, where each dish is a celebration of tradition and taste. From handmade pasta to tantalizing sauces, experience the artistry of Italy's culinary heritage. Buon appetito!
           </p>
           <button className="bg-green font-semibold btn text-white px-8 py-3 rounded-full">
             <a href="/menu">Order Now</a>
